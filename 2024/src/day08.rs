89c98549667436df95237f4e35a95945db76b4d6
David Stangl <david@david-stangl.com>
2024-12-08 14:41:15 +0100

    Clean up 2024/08

--- 2024/src/day08.rs
+++ 2024/src/day08.rs
@@ -8,6 +8,11 @@ pub fn run(input: &str) -> Result<(usize, usize)> {
     let grid: Vec<_> = input.lines().map(|line| line.as_bytes()).collect();
     let height = grid.len();
     let width = grid[0].len();
+    let to_grid_pos = |(x, y): (isize, isize)| {
+        let x = usize::try_from(x).ok()?;
+        let y = usize::try_from(y).ok()?;
+        (x < width && y < height).then_some((x, y))
+    };
 
     let mut antennas: [_; 256] = array::from_fn(|_| vec![]);
     for (y, row) in grid.iter().enumerate() {
@@ -24,33 +29,21 @@ pub fn run(input: &str) -> Result<(usize, usize)> {
             let dx = x2 - x1;
             let dy = y2 - y1;
 
-            let (x3, y3) = (x2 + dx, y2 + dy);
-            if (0..height as isize).contains(&y3) && (0..width as isize).contains(&x3) {
-                antinode[y3 as usize][x3 as usize] |= 1;
+            if let Some((x3, y3)) = to_grid_pos((x2 + dx, y2 + dy)) {
+                antinode[y3][x3] |= 1;
             }
-            let (x4, y4) = (x1 - dx, y1 - dy);
-            if (0..height as isize).contains(&y4) && (0..width as isize).contains(&x4) {
-                antinode[y4 as usize][x4 as usize] |= 1;
+            if let Some((x3, y3)) = to_grid_pos((x1 - dx, y1 - dy)) {
+                antinode[y3][x3] |= 1;
             }
 
             let g = dx.gcd(&dy);
             let dx = dx / g;
             let dy = dy / g;
-            for i in 0.. {
-                let (x3, y3) = (x1 + dx * i, y1 + dy * i);
-                if (0..height as isize).contains(&y3) && (0..width as isize).contains(&x3) {
-                    antinode[y3 as usize][x3 as usize] |= 2;
-                } else {
-                    break;
-                }
-            }
-            for i in 1.. {
-                let (x4, y4) = (x2 - dx * i, y2 - dy * i);
-                if (0..height as isize).contains(&y4) && (0..width as isize).contains(&x4) {
-                    antinode[y4 as usize][x4 as usize] |= 2;
-                } else {
-                    break;
-                }
+
+            let antinodes1 = (0..).map_while(|i| to_grid_pos((x1 + dx * i, y1 + dy * i)));
+            let antinodes2 = (1..).map_while(|i| to_grid_pos((x1 - dx * i, y1 - dy * i)));
+            for (x, y) in antinodes1.chain(antinodes2) {
+                antinode[y as usize][x as usize] |= 2;
             }
         }
     }
