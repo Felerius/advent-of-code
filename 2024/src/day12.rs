9c47df3f90f10c1e102105a168bf15d0b12cc9ef
David Stangl <david@david-stangl.com>
2024-12-12 20:52:27 +0100

    Speed up 2024/12

--- 2024/src/day12.rs
+++ 2024/src/day12.rs
@@ -1,7 +1,6 @@
-use std::mem;
+use std::collections::VecDeque;
 
 use anyhow::Result;
-use utils::hash::{FastHashCollectionExt, FastHashSet};
 
 pub fn run(input: &str) -> Result<(usize, usize)> {
     let grid: Vec<_> = input.lines().map(|line| line.as_bytes()).collect();
@@ -9,7 +8,7 @@ pub fn run(input: &str) -> Result<(usize, usize)> {
     let width = grid[0].len();
 
     let mut seen = vec![vec![false; width]; height];
-    let mut queue = Vec::new();
+    let mut queue = VecDeque::new();
     let mut part1 = 0;
     let mut part2 = 0;
     for (y0, x0) in itertools::iproduct!(0..height, 0..width) {
@@ -18,85 +17,61 @@ pub fn run(input: &str) -> Result<(usize, usize)> {
         }
 
         seen[y0][x0] = true;
-        queue.push((y0, x0));
-        let mut queue_idx = 0;
-        while let Some(&(y, x)) = queue.get(queue_idx) {
-            queue_idx += 1;
-            for (y2, x2) in neighbors(y, x, &grid) {
+        queue.push_back((y0, x0));
+        let mut area = 0;
+        let mut corners = 0;
+        let mut perimeter = 0;
+        while let Some((y, x)) = queue.pop_front() {
+            let neighbors = neighbors(y, x, &grid);
+            let non_diagonal = [0, 2, 4, 6].map(|i| neighbors[i]);
+
+            area += 1;
+            perimeter += non_diagonal.iter().filter(|&&c| c.is_none()).count();
+
+            let outside_corners = (0..4)
+                .filter(|&i| non_diagonal[i].is_none() && non_diagonal[(i + 1) % 4].is_none())
+                .count();
+            let inside_corners = [1, 3, 5, 7]
+                .into_iter()
+                .filter(|&i| {
+                    neighbors[i].is_none()
+                        && neighbors[i - 1].is_some()
+                        && neighbors[(i + 1) % 8].is_some()
+                })
+                .count();
+            corners += outside_corners + inside_corners;
+
+            for (y2, x2) in non_diagonal.into_iter().flatten() {
                 if !seen[y2][x2] && grid[y2][x2] == grid[y][x] {
                     seen[y2][x2] = true;
-                    queue.push((y2, x2));
+                    queue.push_back((y2, x2));
                 }
             }
         }
 
-        let (perimeter1, sides1) = calc_sides(&mut queue);
-        for (y, x) in &mut queue {
-            mem::swap(y, x);
-        }
-        let (perimeter2, sides2) = calc_sides(&mut queue);
-        let area = queue.len();
-        queue.clear();
-
-        part1 += area * (perimeter1 + perimeter2);
-        part2 += area * (sides1 + sides2);
+        part1 += area * perimeter;
+        part2 += area * corners;
     }
 
     Ok((part1, part2))
 }
 
-fn calc_sides(pts: &mut [(usize, usize)]) -> (usize, usize) {
-    pts.sort_unstable();
-    let mut cur = FastHashSet::new();
-    let mut prev = FastHashSet::new();
-    let mut prev_y = usize::MAX - 1;
-    let mut perimeter = 0;
-    let mut sides = 0;
-    for row in pts.chunk_by(|&(y1, _), &(y2, _)| y1 == y2) {
-        let cur_y = row[0].0;
-        if prev_y + 1 != cur_y {
-            prev.clear();
-        }
-        prev_y = cur_y;
-
-        cur.clear();
-        let mut prev_x = None;
-        let mut parity = true;
-        for &(_, x) in row {
-            if prev_x.is_none_or(|prev_x| prev_x + 1 != x) {
-                if let Some(prev_x) = prev_x {
-                    cur.insert((prev_x + 1, parity));
-                    parity = !parity;
-                }
-
-                cur.insert((x, parity));
-                parity = !parity;
-            }
-
-            prev_x = Some(x);
-        }
-
-        if let Some(prev_x) = prev_x {
-            cur.insert((prev_x + 1, parity));
-        }
-
-        sides += cur.difference(&prev).count();
-        perimeter += cur.len();
-        mem::swap(&mut cur, &mut prev);
-    }
-
-    (perimeter, sides)
-}
-
-fn neighbors(y: usize, x: usize, grid: &[&[u8]]) -> impl Iterator<Item = (usize, usize)> {
+fn neighbors(y: usize, x: usize, grid: &[&[u8]]) -> [Option<(usize, usize)>; 8] {
+    let up = y > 0;
+    let right = x + 1 < grid[0].len();
+    let down = y + 1 < grid.len();
+    let left = x > 0;
     [
-        y.checked_sub(1).map(|yi| (yi, x)),
-        x.checked_sub(1).map(|xi| (y, xi)),
-        (y + 1 < grid.len()).then_some((y + 1, x)),
-        (x + 1 < grid[0].len()).then_some((y, x + 1)),
+        up.then(|| (y - 1, x)),
+        (up && right).then(|| (y - 1, x + 1)),
+        right.then(|| (y, x + 1)),
+        (right && down).then(|| (y + 1, x + 1)),
+        down.then(|| (y + 1, x)),
+        (down && left).then(|| (y + 1, x - 1)),
+        left.then(|| (y, x - 1)),
+        (left && up).then(|| (y - 1, x - 1)),
     ]
-    .into_iter()
-    .flatten()
+    .map(|neigh| neigh.filter(|&(y2, x2)| grid[y2][x2] == grid[y][x]))
 }
 
 #[cfg(test)]
