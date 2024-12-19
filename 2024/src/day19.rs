990e59b37c50415f3fdc54c330f75406673ae990
David Stangl <david@david-stangl.com>
2024-12-19 16:05:38 +0100

    2024/19

--- 2024/src/day19.rs
+++ 2024/src/day19.rs
@@ -1,5 +1,61 @@
 use anyhow::Result;
 
-pub fn run(_input: &str) -> Result<()> {
-    Ok(())
+pub fn run(input: &str) -> Result<(usize, usize)> {
+    let mut lines = input.lines();
+    let patterns: Vec<_> = lines
+        .next()
+        .unwrap()
+        .split(", ")
+        .map(|p| p.as_bytes())
+        .collect();
+
+    let (part1, part2) = lines
+        .skip(1)
+        .map(|line| {
+            let line = line.as_bytes();
+            let mut dp = vec![0; line.len() + 1];
+            dp[0] = 1;
+            for i in 1..=line.len() {
+                dp[i] = patterns
+                    .iter()
+                    .map(|&pattern| {
+                        let matches = pattern.len() <= i && &line[i - pattern.len()..i] == pattern;
+                        if matches {
+                            dp[i - pattern.len()]
+                        } else {
+                            0
+                        }
+                    })
+                    .sum();
+            }
+
+            dp[line.len()]
+        })
+        .fold((0, 0), |(part1, part2), count| {
+            (part1 + usize::from(count > 0), part2 + count)
+        });
+
+    Ok((part1, part2))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "\
+r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb";
+
+    #[test]
+    fn test() {
+        assert_eq!(run(INPUT).unwrap(), (6, 16));
+    }
 }
