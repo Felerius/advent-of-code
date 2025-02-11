a50d6d96e47b1446f4616808681927a17e4fa8a7
David Stangl <david@david-stangl.com>
2025-02-11 12:10:26 +0100

    Speed up 2017-12 and input parsing

--- 2017/src/day12.rs
+++ 2017/src/day12.rs
@@ -1,58 +1,35 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
+use utils::input::Input;
 
 pub(crate) fn run(input: &str) -> Result<(usize, usize)> {
-    let n = input.lines().count();
-    let mut dsu = DisjointSetUnion::new(n);
-    for (i, line) in input.lines().enumerate() {
-        debug_assert!(line.starts_with(&format!("{i} <-> ")));
-        let (_, to_str) = line.split_once(" <-> ").context("invalid input line")?;
-        for to in to_str.split(", ") {
-            let to = to.parse().context("invalid number")?;
-            dsu.union(i, to);
+    let lines: Vec<_> = input.lines().collect();
+    let mut seen = vec![false; lines.len()];
+
+    let mut part1 = 0;
+    let mut part2 = 0;
+    for v0 in 0..lines.len() {
+        if !seen[v0] {
+            let size = dfs(v0, &mut seen, &lines)?;
+            if v0 == 0 {
+                part1 = size;
+            }
+            part2 += 1;
         }
     }
 
-    let part1 = dsu.group_size(0);
-    let part2 = (0..n).filter(|&i| dsu.find(i) == i).count();
     Ok((part1, part2))
 }
 
-struct DisjointSetUnion(Vec<isize>);
-
-impl DisjointSetUnion {
-    fn new(n: usize) -> Self {
-        Self(vec![-1; n])
-    }
-
-    fn find(&mut self, i: usize) -> usize {
-        if let Ok(parent) = usize::try_from(self.0[i]) {
-            let root = self.find(parent);
-            self.0[i] = root as isize;
-            root
-        } else {
-            i
-        }
-    }
-
-    fn group_size(&mut self, i: usize) -> usize {
-        let root = self.find(i);
-        -self.0[root] as usize
-    }
-
-    fn union(&mut self, i: usize, j: usize) -> bool {
-        let mut i = self.find(i);
-        let mut j = self.find(j);
-        if i == j {
-            return false;
-        }
-
-        if self.0[i] > self.0[j] {
-            (i, j) = (j, i);
-        }
-        self.0[i] += self.0[j];
-        self.0[j] = i as isize;
-        true
-    }
+fn dfs(v: usize, seen: &mut [bool], lines: &[&str]) -> Result<usize> {
+    seen[v] = true;
+    lines[v][6..]
+        .unsigned_integers()
+        .try_fold(1, |mut cnt, neighbor: usize| {
+            if !seen[neighbor] {
+                cnt += dfs(neighbor, seen, lines)?;
+            }
+            Ok(cnt)
+        })
 }
 
 #[cfg(test)]
--- utils/src/input.rs
+++ utils/src/input.rs
@@ -1,5 +1,8 @@
+use std::{iter::FusedIterator, marker::PhantomData};
+
+use anyhow::{anyhow, Result};
 use arrayvec::ArrayVec;
-use num::{Integer, Signed};
+use num::{Integer, Signed, Unsigned};
 
 pub fn integers<T, const N: usize>(input: impl AsRef<[u8]>) -> [T; N]
 where
@@ -61,4 +64,106 @@ where
 
     nums.into_inner()
         .unwrap_or_else(|nums| panic!("Expected {} integers, got {}", N, nums.len()))
+}
+
+pub trait Input {
+    fn unsigned_integers<T>(&self) -> IntegersUnsigned<'_, T>;
+    fn signed_integers<T>(&self) -> IntegersSigned<'_, T>;
+
+    fn unsigned_integers_n<T, const N: usize>(&self) -> Result<[T; N]>
+    where
+        T: Integer + Unsigned + From<u8>,
+    {
+        try_collect_n(self.unsigned_integers())
+    }
+
+    fn signed_integers_n<T, const N: usize>(&self) -> Result<[T; N]>
+    where
+        T: Integer + Signed + From<u8>,
+    {
+        try_collect_n(self.signed_integers())
+    }
+}
+
+impl<T: AsRef<[u8]> + ?Sized> Input for T {
+    fn unsigned_integers<I>(&self) -> IntegersUnsigned<'_, I> {
+        IntegersUnsigned(self.as_ref(), PhantomData)
+    }
+
+    fn signed_integers<I>(&self) -> IntegersSigned<'_, I> {
+        IntegersSigned(self.as_ref(), PhantomData)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct IntegersUnsigned<'a, T>(&'a [u8], PhantomData<T>);
+
+impl<T: Integer + Unsigned + From<u8>> Iterator for IntegersUnsigned<'_, T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let (num, rem) = next_unsigned::<T>(self.0)?;
+        self.0 = rem;
+        Some(num)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = self.0.len();
+        (0, Some((len + 1) / 2))
+    }
+}
+
+impl<T: Integer + Unsigned + From<u8>> FusedIterator for IntegersUnsigned<'_, T> {}
+
+#[derive(Debug, Clone, Copy)]
+pub struct IntegersSigned<'a, T>(&'a [u8], PhantomData<T>);
+
+impl<T: Integer + Signed + From<u8>> Iterator for IntegersSigned<'_, T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let (num, rem) = next_signed::<T>(self.0)?;
+        self.0 = rem;
+        Some(num)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = self.0.len();
+        (0, Some((len + 1) / 2))
+    }
+}
+
+impl<T: Integer + Signed + From<u8>> FusedIterator for IntegersSigned<'_, T> {}
+
+fn next_unsigned<T: Integer + From<u8>>(s: &[u8]) -> Option<(T, &[u8])> {
+    let start = s.iter().position(u8::is_ascii_digit)?;
+    let mut num = T::zero();
+    let mut s = &s[start..];
+    while let Some((&c, tail)) = s.split_first().filter(|(c, _)| c.is_ascii_digit()) {
+        num = num * T::from(10_u8) + T::from(c - b'0');
+        s = tail;
+    }
+
+    Some((num, s))
+}
+
+fn next_signed<T: Integer + Signed + From<u8>>(s: &[u8]) -> Option<(T, &[u8])> {
+    let start = s.iter().position(u8::is_ascii_digit)?;
+    let (mut num, rem) = next_unsigned::<T>(&s[start..])?;
+    if s[..start].ends_with(b"-") {
+        num = -num;
+    }
+
+    Some((num, rem))
+}
+
+fn try_collect_n<T, const N: usize>(iter: impl Iterator<Item = T>) -> Result<[T; N]> {
+    let mut arr = ArrayVec::<T, N>::new();
+    for num in iter {
+        arr.try_push(num)
+            .map_err(|_| anyhow!("Expected only {N} integers, got at least one more"))?;
+    }
+
+    arr.into_inner()
+        .map_err(|arr| anyhow!("Expected {} integers, got {}", N, arr.len()))
 }
